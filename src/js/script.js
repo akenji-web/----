258a2ad5b18810fa2c3dc0d4d62974114962b832
akenji-web <amkj0827pratra@gmail.com>
2024-03-12 20:43:40 +0900

    初稿提出

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,105 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+  // ハンバーガーメニュー
+  $(".js-hamburger,.js-drawer").click(function () {
+    $(".js-hamburger").toggleClass("is-active");
+    $(".js-drawer").fadeToggle();
+  });
+
+  // スクロール時にヘッダーメニューの背景色を変更
+  $(window).on('scroll', function () {
+    if ($('.mainview').height() < $(this).scrollTop()) {
+      $('.js-header').addClass('change-color');
+    } else {
+      $('.js-header').removeClass('change-color');
+    }
+  });
+
+  // メインビューのスライダー
+  const mainviewswiper = new Swiper(".mainview__swiper", {
+    loop: true,
+    effect: "fade",
+    speed: 1500,
+    allowTouchMove: false,
+    autoplay: {
+      delay: 3000,
+    },
+  });
 
 
+  // Campaignのスライダー
+  const campaignswiper = new Swiper(".campaign__swiper", {
+    loop: true, //繰り返しをする
+    loopedSlides: 4,
+    speed: 500,
+    spaceBetween: 24, //スライド間の距離を24px
+    width: 280,
+    effect: "slide",
+    autoplay: {
+      delay: 2000,
+      waitForTransition: false
+    },
+    navigation: {
+      prevEl: '.swiper-button-next',
+      nextEl: '.swiper-button-prev'
+    },
+    // when window width is >= 768px
+    breakpoints: {
+      768: {
+        spaceBetween: 40, //スライド間の距離を40px
+        width: 333
+      }
+    }
+  });
+
+
+  // 画像表示のアニメーション
+
+  //要素の取得とスピードの設定
+  var box = $('.js-color-box'),
+  speed = 700;
+
+  //.colorboxの付いた全ての要素に対して下記の処理を行う
+  box.each(function(){
+    $(this).append('<div class="color"></div>')
+    var color = $(this).find($('.color')),
+    image = $(this).find('img');
+    var counter = 0;
+
+    image.css('opacity','0');
+    color.css('width','0%');
+    //inviewを使って背景色が画面に現れたら処理をする
+    color.on('inview', function(){
+      if(counter == 0){
+        $(this).delay(200).animate({'width':'100%'},speed,function(){
+            image.css('opacity','1');
+            $(this).css({'left':'0' , 'right':'auto'});
+            $(this).animate({'width':'0%'},speed);
+        })
+        counter = 1;
+      }
+    });
+  });
+
+  // トップへ戻るボタン
+  $(function () {
+    const pageTop = $(".js-page-top");
+    pageTop.hide();
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 20) {
+        pageTop.fadeIn();
+      } else {
+        pageTop.fadeOut();
+      }
+    });
+    pageTop.click(function () {
+      $("body, html").animate(
+        {
+          scrollTop: 0,
+        },
+        300
+      );
+      return false;
+    });
+  });
 });
